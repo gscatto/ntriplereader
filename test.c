3307f21be43774023ce8349b4454e61ca7296f72
Giulio Scattolin <giulio.scattolin@gmail.com>
2024-04-24 08:37:11 +0200

    test: ntriplereader_read() calls ntriplebuilder_reporttokenerror()

--- test.c
+++ test.c
@@ -1,2 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char ntriplebuilderspy_interactions[1024];
+
+void ntriplebuilder_reporttokenerror() {
+  strcat(ntriplebuilderspy_interactions, "reporttokenerror()");
+}
+
+void ntriplereader_read() {
+  ntriplebuilder_reporttokenerror();
+}
+
 int main() {
+  ntriplereader_read();
+  if (strcmp(ntriplebuilderspy_interactions, "reporttokenerror()") != 0) {
+    fprintf(stderr,
+	    "error: expect ntriplebuilderspy_interactions to be \"%s\", got \"%s\"",
+	    "reporttokenerror()",
+	    ntriplebuilderspy_interactions);
+    exit(EXIT_FAILURE);
+  }
 }
